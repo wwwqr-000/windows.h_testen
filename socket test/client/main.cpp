b8140e075f9408fe6a169d38dfc4f3cd1390ce81
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2024-01-22 21:07:06 +0100

    Test met textures

---
+++ socket test/client/main.cpp
@@ -0,0 +1,90 @@
+#include <Windows.h>
+
+HWND hwnd;
+HBITMAP mainBitMap;
+HDC hdcMem; // Declaration of hdcMem
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    else if (uMsg == WM_PAINT) {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        RECT clientRect;
+        GetClientRect(hwnd, &clientRect);
+
+        HBRUSH bgColor = CreateSolidBrush(RGB(210, 210, 210));
+        FillRect(hdc, &clientRect, bgColor);
+        DeleteObject(bgColor);
+
+        if (mainBitMap != NULL) {
+            BITMAP bm;
+            GetObject(mainBitMap, sizeof(BITMAP), &bm);
+            //BitBlt(hdc, 100, 100, 10, 10, hdcMem, 0, 0, SRCCOPY);
+            /**
+            * 1: Positie in window
+            * 2: ImgSize in pixels
+            * 3: offset binnen image
+            */
+            StretchBlt(hdc, 10, 10, 120, 64, hdcMem, 18, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
+
+        }
+
+        EndPaint(hwnd, &ps);
+    }
+    else {
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+    mainBitMap = (HBITMAP)LoadImageW(NULL, L"assets/img/Blocks.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+
+    if (mainBitMap == NULL) {
+        MessageBoxW(NULL, L"Image loading failed!", L"Error", MB_ICONERROR);
+        return 1;
+    }
+
+    hdcMem = CreateCompatibleDC(NULL);
+    SelectObject(hdcMem, mainBitMap);
+
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"MyWindowClass";
+    RegisterClassW(&wc);
+
+    hwnd = CreateWindowExW(0, L"MyWindowClass", L"M", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 500, 500, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL) {
+        MessageBoxW(NULL, L"Window creation failed!", L"Error", MB_ICONERROR);
+        return 1;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+
+    MSG msg;
+    while (true) {
+        InvalidateRect(hwnd, NULL, FALSE);
+        UpdateWindow(hwnd);
+
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        Sleep(16);
+    }
+
+    DeleteDC(hdcMem);
+
+    return 0;
+}
---
+++ socket test/server/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+//Config
+std::string server_ip = "192.168.0.105";
+int server_port = 12345;
+int clientLimit = 4;
+//
+bool serverIsOn = false;
+
+std::vector<SOCKET> clientSockList;
+std::vector<std::thread> clientHandler;
+std::vector<std::thread> listenerThread;
+
+bool wsaKill(SOCKET serverSocket) {
+    closesocket(serverSocket);
+    WSACleanup();
+    return true;
+}
+void threadKill() {
+    for (auto& thread : clientHandler) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+    for (auto& thread : listenerThread) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+}
+
+int clientIndexNoConst(SOCKET clientSocket) {//Initial clientSocket index for data vectors. Not always the same, so no constant var...
+    int tmpCount = -1;
+    for (auto& client : clientSockList) {
+        ++tmpCount;
+        if (clientSocket == client) {
+            break;
+        }
+    }
+    return tmpCount;
+}
+
+void console(SOCKET serverSocket) {
+    while (serverIsOn) {
+        std::string input;
+        std::cin >> input;
+        if (input == "exit") {
+            return;
+        }
+    }
+}
+
+void clienthandlerFunc(SOCKET clientSocket, sockaddr_in clientAddr) {
+    std::string clientUserName = "";
+    if (clientSockList.size() == clientLimit) {
+        std::string tmpFullMsg = "%serverIsFull%";
+        send(clientSocket, tmpFullMsg.c_str(), tmpFullMsg.length(), 0);
+        closesocket(clientSocket);
+        return;
+    }
+    std::cout << "Connection accepted from " << inet_ntoa(clientAddr.sin_addr) << ".\n";
+    clientSockList.push_back(clientSocket);
+    int tmpClientIndex = clientIndexNoConst(clientSocket);
+    std::string okJoinMsg = "%welcomeToServer%";
+    send(clientSocket, okJoinMsg.c_str(), okJoinMsg.length(), 0);
+    while (serverIsOn) {
+        int recBuff = 3000;
+        char buffer[recBuff];
+        int bytesRead = recv(clientSocket, buffer, recBuff, 0);
+        if (bytesRead > 0) {
+            std::string msgFromClient = std::string(buffer, bytesRead);
+            std::cout << msgFromClient << "\n";
+        }
+        else {
+            std::cout << clientUserName << "left the server.\n";
+            break;
+        }
+    }
+}
+
+void listener(SOCKET serverSocket) {//Listening for incoming client connections...
+    while (serverIsOn) {
+        SOCKET clientSocket = accept(serverSocket, 0, 0);//Wait for incoming client...
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "An invalid client socket tried to connect to the server.\n";
+            continue;
+        }
+        sockaddr_in clientAddr;
+        int clientAddrLen = sizeof(clientAddr);
+        getpeername(clientSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        clientHandler.emplace_back([clientSocket, clientAddr]() { clienthandlerFunc(clientSocket, clientAddr); });
+    }
+    wsaKill(serverSocket);
+}
+
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "Could not create winsock setup. Press enter to exit.\n";
+        system("set /p end=");
+        return 1;
+    }
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cout << "This serverSocket is an invalid Socket type. Press enter to exit.\n";
+        system("set /p end=");
+        return 2;
+    }
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(server_ip.c_str());
+    serverAddr.sin_port = htons(server_port);
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == INVALID_SOCKET) {
+        std::cout << "Could not bind the serverSocket. Press enter to exit.\n";
+        system("set /p end=");
+        return 0;
+    }
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cout << "Could not start listening to connections. Press enter to exit.\n";
+        system("set /p end=");
+        return 0;
+    }
+    serverIsOn = true;
+    std::cout << "Success: Server is running on " << server_ip << ":" << server_port << "\n";
+    console(serverSocket);
+    threadKill();
+    wsaKill(serverSocket);
+    return 0;
+}
